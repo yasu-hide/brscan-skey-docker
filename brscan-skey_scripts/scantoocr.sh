fc6f0ba43bfc77e6586dfc62f37952270c8cd7ad
yasu-hide <5038337+yasu-hide@users.noreply.github.com>
2020-05-05 11:00:14 +0900

    tesseractのオプション順序調整

--- brscan-skey_scripts/scantoocr.sh
+++ brscan-skey_scripts/scantoocr.sh
@@ -17,6 +17,6 @@ mkdir -p /tmp/brscan
 sleep  0.1
 output_tmp=`mktemp /tmp/brscan/brscan.XXXXXX`
 echo "Retrieving image from $2 to $output_file."
-scanimage --batch --batch-print --device-name "$device" --mode Gray --resolution $resolution --format $format | tesseract -l jpn -c stream_filelist=true - - pdf > $output_tmp
+scanimage --batch --batch-print --device-name "$device" --mode Gray --resolution $resolution --format $format | tesseract - - -l jpn -c stream_filelist=true pdf > $output_tmp
 mv $output_tmp $output_file && echo .
 /app/bash-onedrive-upload/onedrive-upload $output_file && rm -f $output_file
